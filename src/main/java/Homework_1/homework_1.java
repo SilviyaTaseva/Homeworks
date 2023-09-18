c451d6a11a4eba1f74045e3d2a0b8876e1b3350a
SilviyaTaseva <136969647+SilviyaTaseva@users.noreply.github.com>
2023-09-18 18:53:56 +0300

    корекция на домашна 1

---
+++ src/main/java/Homework_1/homework_1.java
@@ -0,0 +1,18 @@
+package Homework_1;
+import java.util.Scanner;
+public class homework_1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello, what is your name ? ");
+        String name = sc.nextLine();
+
+        System.out.println("How do you spend your free time?");
+        String hobby = sc.nextLine();
+
+        System.out.println("Do you like to read through me ?");
+        String answer = sc.nextLine();
+    }
+}
+
