3b3731932ef8f29c5e191a51b0cf3775c5264618
SilviyaTaseva <136969647+SilviyaTaseva@users.noreply.github.com>
2023-09-19 23:15:47 +0300

    лице на триъгълник

---
+++ src/main/java/mathematics/TriangleArea.java
@@ -0,0 +1,26 @@
+package mathematics;
+import java.util.Scanner;
+
+public class TriangleArea {
+    public static void main(String[]args){
+     //Triangle Area formula : S= square of p(p−a)(p−b)(p−c), where p = a+b+c/2
+
+        Scanner ta = new Scanner(System.in);
+        int A,B,C,p,triangleArea;
+
+         System.out.println("Enter side A  : ");
+          A = ta.nextInt();
+
+          System.out.println("Enter side B : ");
+           B = ta.nextInt();
+
+         System.out.println("Enter side C ; ");
+           C = ta.nextInt();
+
+           p = A+B+C/2;
+
+          triangleArea = (int) Math.sqrt(p*(p-A)*p*(p-B)*p*(p-C));
+
+          System.out.println("The triangle area is :" + triangleArea);
+          }
+}
