67e7c0ab215e3003677ca8c75b97b5255f0c7874
SilviyaTaseva <136969647+SilviyaTaseva@users.noreply.github.com>
2023-10-25 16:06:37 +0300

    лице на триъгълник

---
+++ src/main/java/homework_5/Password.java
@@ -0,0 +1,21 @@
+package homework_5;
+import com.sun.jdi.IntegerValue;
+
+import java.util.Scanner;
+import java.util.Arrays;
+public class Password {
+
+    public static void main(String[] args){
+        System.out.println(isThePasswordValid("Pa$$w0rd"));
+    }
+    public static boolean isThePasswordValid(String password){
+        String regex = "\\A(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}\\z";
+        return password.matches(regex);
+    }
+}
+
+
+
+
+
+
